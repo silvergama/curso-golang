7926ca0804139d467c66f961b4fedd2ebf846884
Silver Gama <silver.mdg@gmail.com>
2020-03-04 22:04:16 -0300

    Aula 1 - Fundamentos - Comandos terminal

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro programa em %s! \n", "Go")
+}
+
+/*
+Nessa aula foi passados alguns comandos do terminal, por ex.:
+go get
+go version
+go vet
+go env
+go run
+*/
