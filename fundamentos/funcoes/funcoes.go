044cb0d50955b436474a76de215a8249dfdc27ad
Silver Gama <silver.mdg@gmail.com>
2020-03-07 23:39:51 -0300

    Aula 1 - Fundamentos - Funcões básicas

---
+++ fundamentos/funcoes/funcoes.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func somar(x, y int) int {
+	return x + y
+}
+
+func imprimir(valor int) {
+	fmt.Println(valor)
+}
---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+* Para testar o código...
+* executar go run *.go
+* se executar go run main.go ocorrerá um erro: undefined somar e undefined imprimir
+ */
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
