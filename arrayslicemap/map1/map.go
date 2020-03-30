4f575390e3c1e06c24d71d8981c79c33054efc8e
Silver Gama <silver.mdg@gmail.com>
2020-03-29 22:37:17 -0300

    Seção 5 - Array/Slices/Map - Trabalhando com Maps #01

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678911] = "Maria"
+	aprovados[12345678912] = "Pedro"
+	aprovados[12345678913] = "Ana"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[12345678913])
+	delete(aprovados, 12345678913)
+	fmt.Println(aprovados[12345678913])
+	
+
+}
